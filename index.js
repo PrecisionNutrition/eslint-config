50f9e0d946ee50e3975e2f52132edba180bba137
James Herdman <james.herdman@gmail.com>
2019-11-05 19:13:18 +0000

    Include a Fix For Indent Rule
    https://github.com/babel/babel-eslint/issues/681#issuecomment-451336031

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ module.exports = {
         'FunctionExpression': { 'parameters': 1, 'body': 1 },
         'ObjectExpression': 1,
         'SwitchCase': 1,
+
+        // https://github.com/babel/babel-eslint/issues/681#issuecomment-451336031
+        'ignoredNodes': ['TemplateLiteral'],
       },
     ],
     'no-constant-condition': 'off',
